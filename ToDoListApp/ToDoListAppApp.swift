1c966bc7028657a44fdf1f0f13b18dd534e5f2ab
Ulugbek <ulu96ek06@gmail.com>
2025-09-13 22:45:57 +0500

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Ulugbek Ilhomov on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
