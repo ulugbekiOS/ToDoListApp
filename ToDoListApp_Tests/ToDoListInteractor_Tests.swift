4bbcec61651eeb908a2f51e50942b67fd8fdb910
Ulugbek <ulu96ek06@gmail.com>
2025-09-19 01:57:41 +0500

    Deleted comments for making code clear

--- ToDoListApp_Tests/ToDoListInteractor_Tests.swift
+++ ToDoListApp_Tests/ToDoListInteractor_Tests.swift
@@ -23,54 +23,35 @@ final class ToDoListInteractorTests: XCTestCase {
     }
     
     func testCreateNewTodo() {
-        // Act
         let todo = interactor.createNewTodo()
-        
-        // Assert
         XCTAssertNotNil(todo)
         XCTAssertEqual(todo.completed, false)
         XCTAssertNotNil(todo.savedDate)
     }
     
     func testEditTodoUpdatesDescription() {
-        // Arrange
         let todo = interactor.createNewTodo()
-        
-        // Act
         interactor.editToDo(entity: todo, newTaskDescription: "New Task")
-        
-        // Assert
         XCTAssertEqual(todo.taskDescription, "New Task")
         XCTAssertEqual(todo.completed, false)
     }
     
     func testDeleteTodoRemovesEntity() {
-        // Arrange
         let todo = interactor.createNewTodo()
         interactor.applyChanges()
         let initialCount = interactor.savedEntities.count
-        
-        // Act
         interactor.deleteToDo(entity: todo)
-        
-        // Assert
         XCTAssertLessThan(interactor.savedEntities.count, initialCount)
     }
     
     func testFilteredEntitiesWithSearchText() {
-        // Arrange
         let todo1 = interactor.createNewTodo()
         todo1.taskDescription = "Buy milk"
         let todo2 = interactor.createNewTodo()
         todo2.taskDescription = "Go gym"
         interactor.applyChanges()
-        
         interactor.searchText = "milk"
-        
-        // Act
         let filtered = interactor.filteredEntities
-        
-        // Assert
         XCTAssertEqual(filtered.count, 1)
         XCTAssertEqual(filtered.first?.taskDescription, "Buy milk")
     }
